73cdd6279049ee7a2aff943e1e7e26d72de3ee62
Kian Kasad <kian@kasad.com>
2022-10-20 23:09:04 -0700

    Make 'join' page redirect to new recruiting page

---
+++ content/join/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * This page will redirect to the url specified by the $url variable below.
+ */
+
+$url = 'https://www.bsatroop834.org/about-834/how-we-differ';
+header('Location: ' . $url);
+die();
+
+?>
